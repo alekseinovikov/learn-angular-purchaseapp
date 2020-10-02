4e761eaec48b151b3247d664d881463b83dfff92
Aleksei Novikov <novikov.aleksei.92@gmail.com>
2020-10-02 16:15:38 +0300

    Binding child properties (output)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,17 @@
 import {Component} from '@angular/core';
-import {v4 as uuid} from 'uuid';
-
-class Item {
-    id: string;
-    purchase: string;
-    done: boolean;
-    price: number;
-
-    constructor(purchase: string, price: number) {
-        this.id = uuid();
-        this.purchase = purchase;
-        this.price = price;
-        this.done = false;
-    }
-}
+import {Test} from "./child.component";
 
 @Component({
     selector: 'purchase-app',
     template: `
-        <child-comp [userName]="name" [userAge]="age"></child-comp>
-        <input type="text" [(ngModel)]="name"/>
-        <input type="number" [(ngModel)]="age"/>
+        <h2>Количество кликов: {{clicks}}</h2>
+        <child-comp (onChanged)="onChanged($event)"></child-comp>
     `
 })
 export class AppComponent {
-    name: string = "Tom";
-    age: number = 24;
+    clicks: number = 0;
+
+    onChanged(increased: Test) {
+        this.clicks += increased.count;
+    }
 }
--- src/app/child.component.ts
+++ src/app/child.component.ts
@@ -1,43 +1,27 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
+
+export class Test {
+    constructor(public count: number) {
+    }
+}
 
 @Component({
     selector: 'child-comp',
-    template: `<p>Имя пользователя: {{userName}}</p>
-    <p>Возраст пользователя: {{userAge}}</p>`,
-    styles: [`h2, p {
-        color: red;
-    }`]
+    template: `
+        <button (click)="change(true)">+</button>
+        <button (click)="change(false)">-</button>
+    `
 })
 export class ChildComponent {
 
-    _userName: string;
-    _userAge: number;
+    @Output() onChanged: EventEmitter<Test> = new EventEmitter<Test>();
 
-    @Input()
-    set userName(userName: string) {
-        this._userName = userName;
-    }
-
-    get userName(): string {
-        return this._userName;
-    }
-
-    @Input()
-    set userAge(age: number) {
-        if (age < 0) {
-            this._userAge = 0;
-            return;
+    change(increased: boolean) {
+        if (increased) {
+            this.onChanged.emit(new Test(1));
+        } else {
+            this.onChanged.emit(new Test(-1));
         }
-
-        if (age > 100) {
-            this._userAge = 100;
-            return;
-        }
-
-        this._userAge = age;
     }
 
-    get userAge(): number {
-        return this._userAge;
-    }
 }
