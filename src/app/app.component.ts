1a42afb9ca9f4b515064e120e510fcf4009bc10c
Aleksei Novikov <novikov.aleksei.92@gmail.com>
2020-10-02 15:57:38 +0300

    Binding child properties (input)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,78 +18,12 @@ class Item {
 @Component({
     selector: 'purchase-app',
     template: `
-        <child-comp>Total sum is: {{sum}}</child-comp>
-        <div class="page-header">
-            <h1> Список покупок </h1>
-        </div>
-        <div class="panel">
-            <div class="form-inline">
-                <div class="form-group">
-                    <div class="col-md-8">
-                        <input class="form-control" [(ngModel)]="text" placeholder="Название"/>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <div class="col-md-6">
-                        <input type="number" class="form-control" [(ngModel)]="price" placeholder="Цена"/>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <div class="col-md-offset-2 col-md-8">
-                        <button class="btn btn-default" (click)="addItem(text, price)">Добавить</button>
-                    </div>
-                </div>
-            </div>
-            <table class="table table-striped">
-                <thead>
-                <tr>
-                    <th>Предмет</th>
-                    <th>Цена</th>
-                    <th>Куплено</th>
-                    <th>Удалить</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr *ngFor="let item of items">
-                    <td>{{item.purchase}}</td>
-                    <td>{{item.price}}</td>
-                    <td><input type="checkbox" [(ngModel)]="item.done"/></td>
-                    <td>
-                        <button class="btn btn-default" (click)="delete(item.id)">X</button>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        </div>`
+        <child-comp [userName]="name" [userAge]="age"></child-comp>
+        <input type="text" [(ngModel)]="name"/>
+        <input type="number" [(ngModel)]="age"/>
+    `
 })
 export class AppComponent {
-    text: string;
-    price: number = 0;
-
-    get sum(): number {
-        return this.items
-            .map(i => i.price)
-            .reduce((a, b) => a + b, 0);
-    }
-
-    items: Item[] =
-        [
-            new Item("Хлеб", 15.9),
-            new Item("Масло", 15.9),
-        ];
-
-    addItem(text: string, price: number): void {
-
-        if (text == null || text.trim() == "" || price == null)
-            return;
-        this.items.push(new Item(text, price));
-
-        this.text = '';
-        this.price = 0;
-    }
-
-    delete(id: string) {
-        this.items = this.items.filter(i => i.id != id)
-    }
-
+    name: string = "Tom";
+    age: number = 24;
 }
--- src/app/child.component.ts
+++ src/app/child.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
     selector: 'child-comp',
-    template: `
-            <ng-content></ng-content>
-        <h2>Welcome, {{name}}!</h2>`,
-    styles: [`h2, p {color:red;}`]
+    template: `<p>Имя пользователя: {{userName}}</p>
+    <p>Возраст пользователя: {{userAge}}</p>`,
+    styles: [`h2, p {
+        color: red;
+    }`]
 })
 export class ChildComponent {
-    name= "Aleksei";
+
+    _userName: string;
+    _userAge: number;
+
+    @Input()
+    set userName(userName: string) {
+        this._userName = userName;
+    }
+
+    get userName(): string {
+        return this._userName;
+    }
+
+    @Input()
+    set userAge(age: number) {
+        if (age < 0) {
+            this._userAge = 0;
+            return;
+        }
+
+        if (age > 100) {
+            this._userAge = 100;
+            return;
+        }
+
+        this._userAge = age;
+    }
+
+    get userAge(): number {
+        return this._userAge;
+    }
 }
