bd2dc0dccbc26bc79b0411b0ebfca5b175017b80
freedom4live <novikov.aleksei.92@gmail.com>
2020-10-07 20:12:36 +0300

    Style and class dirrectives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,45 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'purchase-app',
     template: `
-        <div>
-            <h1>AppModule</h1>
-            <data-comp></data-comp>
+        <div [ngClass]="{invisible: visibility}">
+            <h1>Hello Angular 10</h1>
+            <p [ngStyle]="{'font-size':'14px', 'font-family':'Segoe Print'}">
+                Angular 10 представляет модульную архитектуру приложения
+            </p>
         </div>
-    `
+        <button (click)="toggle()">Toggle</button>
+        
+    `,
+    styles: [`
+        .verdanaFont {
+            font-size: 13px;
+            font-family: Verdana;
+        }
+
+        .navyColor {
+            color: navy;
+        }
+
+        .invisible {
+            display: none;
+        }
+    `]
 })
 export class AppComponent {
+
+    isVeranda = true;
+    isNavy = true;
+
+    visibility: boolean = true;
+
+    toggle() {
+        this.visibility = !this.visibility;
+    }
+
+
+    currentClasses = {
+        verdanaFont: this.isVeranda,
+        navyColor: this.isNavy
+    }
+
 }
