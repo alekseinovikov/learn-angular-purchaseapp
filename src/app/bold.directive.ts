8de23f769125888b7baab3db50984141951919fd
Aleksei Novikov <novikov.aleksei.92@gmail.com>
2020-10-08 16:40:50 +0300

    Directives listeners and binding

--- src/app/bold.directive.ts
+++ src/app/bold.directive.ts
@@ -1,11 +1,31 @@
-import {Directive, ElementRef, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Renderer2} from '@angular/core';
 
 @Directive({
     selector: '[bold]'
 })
 export class BoldDirective {
 
-    constructor(private elementRef: ElementRef, private renderer: Renderer2) {
-        this.renderer.setStyle(this.elementRef.nativeElement, "font-weight", "bold");
+    private fontWeight = "normal";
+
+    constructor(private element: ElementRef, private renderer: Renderer2){
+        this.renderer.setStyle(this.element.nativeElement, "cursor", "pointer");
     }
+
+
+    @HostBinding("style.fontWeight") get getFontWeight() {
+        return this.fontWeight;
+    }
+
+    @HostBinding("style.cursor") get getCursor() {
+        return "pointer";
+    }
+
+    @HostListener("mouseenter") onMouseEnter() {
+        this.fontWeight = "bold";
+    }
+
+    @HostListener("mouseleave") onMouseLeave() {
+        this.fontWeight = "normal";
+    }
+
 }
